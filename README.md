ee3f293623a2a7eab50a2e6fe03b598253023afe
mwcolem <mwcolem@gmail.com>
2017-02-18 12:09:39 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Django "hello world" app based on tutorial at https://scotch.io/tutorials/build-your-first-python-and-django-application
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/coleman/Development/dev/pythondev/djangohello/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
